60bef922f92b34cf94300c2bb6972c340066cb10
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-05-25 22:31:03 +0530

    feat: Add Flex component for displaying 20 flex items in React

---
+++ Lec6_React/client/src/flexbox/Flex.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./index.css" 
+
+const Flex= () => {
+  return (
+    <>
+      <section className="container">
+        <section className="flex-items">1</section>
+        <section className="flex-items">2</section>
+        <section className="flex-items">3</section>
+        <section className="flex-items">4</section>
+        <section className="flex-items">5</section>
+        <section className="flex-items">6</section>
+        <section className="flex-items">7</section>
+        <section className="flex-items">8</section>
+        <section className="flex-items">9</section>
+        <section className="flex-items">10</section>
+        <section className="flex-items">11</section>
+        <section className="flex-items">12</section>
+        <section className="flex-items">13</section>
+        <section className="flex-items">14</section>
+        <section className="flex-items">15</section>
+        <section className="flex-items">16</section>
+        <section className="flex-items">17</section>
+        <section className="flex-items">18</section>
+        <section className="flex-items">19</section>
+        <section className="flex-items">20</section>
+      </section>
+    </>
+  )
+}
+
+export default Flex
