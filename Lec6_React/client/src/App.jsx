ee83ec186df5b5d6f7ef1b103d077cd53a2b5577
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-05-25 22:30:21 +0530

    feat: Integrate React Router links and components in App.jsx

--- Lec6_React/client/src/App.jsx
+++ Lec6_React/client/src/App.jsx
@@ -1,23 +1,27 @@
-import { useState } from 'react';
-import ClassComponent from './ClassComponent';
-import logo from './logo.svg';
-import FunctionalComponent from './FunctionalComponent';
-
+import { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+import TestComponent from "./Components/TestComponent";
 
 function App() {
-  const [name, setName] = useState("Prabhjeet")
-  const [displayComponent, setdisplayComponent] = useState(true)
-  setTimeout(()=>{setName("Nikhil")},5*1000)
-  const user = {
-    name:"Nikhil",
-    userId:101
-  }
-  return (<>
-    {/* {displayComponent && <ClassComponent name={name} user={user} />} */}
-    <FunctionalComponent name={name} user={user} />
-  </>
-
-  );
+	const [name, setName] = useState("Prabhjeet");
+	const [displayComponent, setdisplayComponent] = useState(true);
+	setTimeout(() => {
+		setName("Nikhil");
+	}, 5 * 1000);
+	const user = {
+		name: "Nikhil",
+		userId: 101,
+	};
+	return (
+		<>
+			{/* {displayComponent && <ClassComponent name={name} user={user} />} */}
+			<Link to="route1" element={<TestComponent />}>
+				Test Route "route1"
+			</Link><br />
+			<Link to="route2/p123?search=Mobile&lang=EN" >Query Params UseCase</Link>
+			<Outlet />
+		</>
+	);
 }
 
 export default App;
