d12682efc95b410860496faeb0a93989c72434c8
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-05-25 22:30:31 +0530

    refactor: Move Link import to FunctionalComponent.jsx for better organization

--- Lec6_React/client/src/Components/FunctionalComponent.jsx
+++ Lec6_React/client/src/Components/FunctionalComponent.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const FunctionalComponent = (props) => {
   const [name, setName] = useState(props.name)
@@ -16,6 +17,7 @@ console.log("all useEffect (componentDidUpdate)");
     <>
     <h1>hi {name} {id}</h1>
     <button onClick={()=>setId(id+1)}>click me</button>
+    <Link to="fromLink" >fromLink</Link>
     </>
   )
 }
