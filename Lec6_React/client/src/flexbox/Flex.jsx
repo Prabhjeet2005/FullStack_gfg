4add3d03bef8d828b29caca8b1bf8b2f624f6af3
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-05-27 09:41:50 +0530

    refactor: Update Flex component to use React Bootstrap Container for layout

--- Lec6_React/client/src/flexbox/Flex.jsx
+++ Lec6_React/client/src/flexbox/Flex.jsx
@@ -1,33 +1,32 @@
 import React from 'react'
 import "./index.css" 
+import {Container} from "react-bootstrap"
 
 const Flex= () => {
   return (
-    <>
-      <section className="container">
-        <section className="flex-items">1</section>
-        <section className="flex-items">2</section>
-        <section className="flex-items">3</section>
-        <section className="flex-items">4</section>
-        <section className="flex-items">5</section>
-        <section className="flex-items">6</section>
-        <section className="flex-items">7</section>
-        <section className="flex-items">8</section>
-        <section className="flex-items">9</section>
-        <section className="flex-items">10</section>
-        <section className="flex-items">11</section>
-        <section className="flex-items">12</section>
-        <section className="flex-items">13</section>
-        <section className="flex-items">14</section>
-        <section className="flex-items">15</section>
-        <section className="flex-items">16</section>
-        <section className="flex-items">17</section>
-        <section className="flex-items">18</section>
-        <section className="flex-items">19</section>
-        <section className="flex-items">20</section>
-      </section>
-    </>
-  )
+		<Container fluid className="flex-container">
+			<section className="flex-items">1</section>
+			<section className="flex-items">2</section>
+			<section className="flex-items">3</section>
+			<section className="flex-items">4</section>
+			<section className="flex-items">5</section>
+			<section className="flex-items">6</section>
+			<section className="flex-items">7</section>
+			<section className="flex-items">8</section>
+			<section className="flex-items">9</section>
+			<section className="flex-items">10</section>
+			<section className="flex-items">11</section>
+			<section className="flex-items">12</section>
+			<section className="flex-items">13</section>
+			<section className="flex-items">14</section>
+			<section className="flex-items">15</section>
+			<section className="flex-items">16</section>
+			<section className="flex-items">17</section>
+			<section className="flex-items">18</section>
+			<section className="flex-items">19</section>
+			<section className="flex-items">20</section>
+		</Container>
+	);
 }
 
 export default Flex
