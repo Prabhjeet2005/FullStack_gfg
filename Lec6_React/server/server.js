eab754e6e0965832c88916baab3502fa77242a38
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-06-08 19:58:08 +0530

    feat: implement server setup with Express and routing for user and cart

---
+++ Lec6_React/server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = require("./routes/router");
+const cartRouter = require("./routes/cartRouter");
+const userRouter = require("./routes/userRouter");
+
+const app = express();
+
+// app.use() -> configure Middleware
+app.use(express.json()); // Parses the incoming body [Middleware Enabled]
+
+// app.get("/checkserver/:id/:type", (req, res) => {
+// 	console.log(req.path);
+// 	console.log(req.params);
+// 	console.log(req.params.id);
+// 	console.log(req.query);
+// 	console.log(req.body);
+
+// 	res.status(200);
+// 	res.send({ success: true, message: "Server is running" });
+// });
+
+app.use("/user",userRouter)
+app.use("/cart",cartRouter)
+app.use("/test",router)
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+	console.log(`Server Running on ${PORT}`);
+});
