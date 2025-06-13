61efb0dac29096c81b82b2c060c79db09b7a4b8c
Prabhjeet <prabhjeetsandhu010@gmail.com>
2025-06-13 21:45:09 +0530

    feat: add delete and update name routes for user management

--- Lec6_React/server/routes/userRouter.js
+++ Lec6_React/server/routes/userRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { signupController, loginController } = require("../controllers/userController");
+const { signupController, loginController, deleteController, updateName } = require("../controllers/userController");
 const userRouter = express.Router()
 
 userRouter.post("/signup",signupController)
 userRouter.post("/login",loginController)
+userRouter.delete("/delete",deleteController)
+userRouter.patch("/updateName",updateName)
 
 module.exports = userRouter;
